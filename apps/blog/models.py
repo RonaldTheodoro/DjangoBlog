0f1de86f104deb8da9dcbfe876df151caa75ccf6
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-04 01:28:51 -0300

    add Post model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICE = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField('title', max_length=250)
+    slug = models.SlugField('slug', max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(
+        'User', verbose_name='author', related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        'status', max_length=10, choices=STATUS_CHOICE, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
